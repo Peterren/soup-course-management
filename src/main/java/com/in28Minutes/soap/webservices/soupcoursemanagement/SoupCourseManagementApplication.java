3deeea165584b287e09ef48032896ebb10d6444d
Yincheng Ren <yr5ka@virginia.edu>
2019-05-16 16:38:04 -0400

    Initial commit

---
+++ src/main/java/com/in28Minutes/soap/webservices/soupcoursemanagement/SoupCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.soap.webservices.soupcoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoupCourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoupCourseManagementApplication.class, args);
+	}
+
+}
